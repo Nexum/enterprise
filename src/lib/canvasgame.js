757914e8b1a4900524355c3b75e0ae014f177f13
Dominik Kienzler <dominik.kienzler@vonaffenfels.de>
2017-12-11 16:59:19 +0100

    added vibrate

--- src/lib/canvasgame.js
+++ src/lib/canvasgame.js
@@ -379,6 +379,10 @@ module.exports = class CanvasGame {
             x: -400 * this.scaleFactorWidth,
             y: 300 * this.scaleFactorHeight
         }, time, Phaser.Easing.Linear.None, true, 0, 0, false);
+
+        if ("vibrate" in navigator) {
+            navigator.vibrate(time);
+        }
     }
 
     _update() {
